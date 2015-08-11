d2506f443b154ed574752cc39751d360ddef4aa7
中软mini025 <zhongruanmini025@zhongruanmini025deMac-mini.local>
2015-08-11 08:56:05 +0800

    Initial Commit

---
+++ ti/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ti
+//
+//  Created by 中软mini025 on 15/8/11.
+//  Copyright (c) 2015年 中软mini025. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
